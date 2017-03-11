0e98d87de44a8edf9612f90312c46c06b6b6b1ef
AgustinGonzalez <xenrak@gmail.com>
2017-03-11 19:58:52 -0300

    Initial Setup

---
+++ bot.config.js
@@ -0,0 +1,6 @@
+var Configuration = {
+    DiscordToken    : "YOUR TOKEN HERE",
+    RiotToken       : "YOUR TOKEN HERE"
+}
+
+module.exports = Configuration;
---
+++ main.js
@@ -0,0 +1,18 @@
+const Configuration = require('./bot.config.js')
+
+console.log(Configuration.DiscordToken);
+
+//const Discord = require("discord.js");
+//const client = new Discord.Client();
+//
+//client.on('ready', () => {
+//  console.log(`Logged in as ${client.user.username}!`);
+//});
+//
+//client.on('message', msg => {
+//  if (msg.content === 'ping') {
+//    msg.reply('Pong!');
+//  }
+//});
+//
+//client.login('token');
